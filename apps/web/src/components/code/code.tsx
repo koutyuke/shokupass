ed8ef0944ca610101d4e59becef1a98f6815f13f
koutyuke <gunji.kousuke08@gmail.com>
2023-12-14 11:22:47 +0900

    feat: ⚰️ (web) 不要なコンポーネントの削除

--- apps/web/src/components/code/code.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-type Props = { children: React.ReactNode; className: string };
-
-const Code: FC<Props> = ({ children, className }) => <code className={className}>{children}</code>;
-
-export { Code };
