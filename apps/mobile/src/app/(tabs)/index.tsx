4c36bc57d95046983232a4c93d49fe959ba6a825
koutyuke <gunji.kousuke08@gmail.com>
2023-12-14 11:41:06 +0900

    refactor: ♻️ (mobile/app) リファクタリング

--- apps/mobile/src/app/(tabs)/index.tsx
+++ apps/mobile/src/app/(tabs)/index.tsx
@@ -3,18 +3,6 @@ import { StyleSheet } from "react-native";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container} className="bg-red-400">
-      <Text style={styles.title} className="bg-red-400">
-        Tab One
-      </Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -31,3 +19,15 @@ const styles = StyleSheet.create({
     width: "80%",
   },
 });
+
+const TabOneScreen = () => (
+  <View style={styles.container} className="bg-red-400">
+    <Text style={styles.title} className="bg-red-400">
+      Tab One
+    </Text>
+    <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <EditScreenInfo path="app/(tabs)/index.tsx" />
+  </View>
+);
+
+export default TabOneScreen;
