67fb6b15fa0d74d26bfa55104288e1a2f8a8733f
koutyuke <gunji.kousuke08@gmail.com>
2023-12-15 16:30:24 +0900

    feat: 🎈 (mobile/app) signInボタン変更に伴う呼び出しの変更

--- apps/mobile/src/app/(tabs)/three.tsx
+++ apps/mobile/src/app/(tabs)/three.tsx
@@ -1,11 +1,12 @@
 import { View } from "react-native";
 import { AuthUser } from "@/components/user";
-import { SignInButton } from "@/features/signIn/components/signIn";
-import { SignOutButton } from "@/features/signIn/components/signOut";
+import { GoogleSignInButton, DiscordSignInButton } from "@/features/signIn/components/signIn";
+import { SignOutButton } from "@/features/signIn/components/signOut/signOut";
 
 const App = () => (
   <View className="flex-1 items-center justify-center bg-gray-100">
-    <SignInButton />
+    <GoogleSignInButton />
+    <DiscordSignInButton />
     <SignOutButton />
     <AuthUser />
   </View>
