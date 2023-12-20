9f32b18363265817b9e2d267d498a127c67e8952
koutyuke <gunji.kousuke08@gmail.com>
2023-12-20 12:28:59 +0900

    feat: 🎈 user情報を出力できる機能を追加

--- apps/mobile/src/components/user.tsx
+++ apps/mobile/src/components/user.tsx
@@ -1,16 +1,18 @@
-import { User } from "@supabase/supabase-js";
+import { Session, User } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
-import { Text } from "react-native";
+import { Button, Text } from "react-native";
 import { supabase } from "@/utils/supabase";
 
 const AuthUser = () => {
   const [user, setUser] = useState<User | null>(null);
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange(async (_, session) => {
-      setUser(session?.user ?? null);
+    } = supabase.auth.onAuthStateChange(async (_, afterSession) => {
+      setUser(afterSession?.user ?? null);
+      setSession(afterSession);
     });
 
     return () => {
@@ -18,7 +20,17 @@ const AuthUser = () => {
     };
   }, []);
 
-  return <Text className="mt-10 text-xl text-gray-500">{user?.id ?? "no user"}</Text>;
+  return (
+    <>
+      <Text className="mt-10 text-xl text-gray-500">{user?.id ?? "no user"}</Text>
+      <Button
+        onPress={() => {
+          console.log(JSON.stringify(session, null, 2));
+        }}
+        title="show session"
+      />
+    </>
+  );
 };
 
 export { AuthUser };
