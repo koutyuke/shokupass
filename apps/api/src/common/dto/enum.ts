7a239bf29e4fb0d60807a843d39acff578d8ad28
koutyuke <gunji.kousuke08@gmail.com>
2023-12-22 09:47:20 +0900

    feat: 🎈 (enum) menuのステータスを追加

--- apps/api/src/common/dto/enum.ts
+++ apps/api/src/common/dto/enum.ts
@@ -1,9 +1,16 @@
 export const enum Role {
   ADMIN = "ADMIN",
+  MODERATOR = "MODERATOR",
   USER = "USER",
 }
 
 export const enum Provider {
   GOOGLE = "GOOGLE",
   DISCORD = "DISCORD",
+}
+
+export const enum MenuStatus {
+  RELEASED = "RELEASED",
+  PREPARATION = "PREPARATION",
+  DELETED = "DELETED",
 }
