56447bfb1710dadfad4e080d7e6a46d5082cae7f
koutyuke <gunji.kousuke08@gmail.com>
2023-12-22 09:50:18 +0900

    feat: 🎈 (menu.module) menuのmoduleの作成

---
+++ apps/api/src/modules/menu/menu.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { InjectToken } from "src/common/constant/injectToken";
+import { MenuController } from "./menu.controller";
+import { MenuRepository } from "./menu.repository";
+import { MenuUseCase } from "./menu.use-case";
+
+@Module({
+  imports: [],
+  controllers: [MenuController],
+  providers: [
+    {
+      provide: InjectToken["MENU_REPOSITORY"],
+      useClass: MenuRepository,
+    },
+    MenuUseCase,
+  ],
+})
+export class MenuModule {}
