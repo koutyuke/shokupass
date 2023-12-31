1f8026241f33362c518019abe6b390fd7b0b721f
koutyuke <gunji.kousuke08@gmail.com>
2023-12-31 22:44:51 +0900

    feat: 🎈 (api/user) userのorder用のapi-end-pointの追加

--- apps/api/src/modules/user/user.module.ts
+++ apps/api/src/modules/user/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { InjectToken } from "src/common/constant/injectToken";
+import { OrderModule } from "../order/order.module";
 import { UserController } from "./user.controller";
 import { UserRepository } from "./user.repository";
 import { UserUseCase } from "./user.use-case";
 
 @Module({
-  imports: [],
+  imports: [OrderModule],
   controllers: [UserController],
   providers: [
     {
