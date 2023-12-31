c172e1d9f9565fb152ad139aec536730fcbbfa33
koutyuke <gunji.kousuke08@gmail.com>
2023-12-31 15:31:49 +0900

    build: 📦️ (db) dbの更新

---
+++ apps/api/prisma/migrations/20231223055622_init/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - The values [UNPAID,UNRECEIVED,CANCELED,EXPIRED] on the enum `OrderStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "OrderStatus_new" AS ENUM ('PENDING', 'PAYMENT', 'COOKING', 'READY_FOR_PICKUP', 'REFUNDING', 'COMPLETED', 'CANCELLED');
+ALTER TABLE "orders" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "orders" ALTER COLUMN "status" TYPE "OrderStatus_new" USING ("status"::text::"OrderStatus_new");
+ALTER TYPE "OrderStatus" RENAME TO "OrderStatus_old";
+ALTER TYPE "OrderStatus_new" RENAME TO "OrderStatus";
+DROP TYPE "OrderStatus_old";
+ALTER TABLE "orders" ALTER COLUMN "status" SET DEFAULT 'PENDING';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "orders" ALTER COLUMN "status" SET DEFAULT 'PENDING';
---
+++ apps/api/prisma/migrations/20231223060145_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterEnum
+ALTER TYPE "MenuStatus" ADD VALUE 'UNRELEASED';
