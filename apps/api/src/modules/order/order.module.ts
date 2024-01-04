97bc0fb313b92c34a392434d75dcdec65fa4c966
koutyuke <gunji.kousuke08@gmail.com>
2024-01-05 05:06:18 +0900

    feat: 🎈 (api/order) lockerModuleの追加

--- apps/api/src/modules/order/order.module.ts
+++ apps/api/src/modules/order/order.module.ts
@@ -1,6 +1,7 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { InjectToken } from "src/common/constant/injectToken";
 import { PaypayModule } from "src/common/paypay/paypay.module";
+import { LockerModule } from "../locker/locker.module";
 import { MenuModule } from "../menu/menu.module";
 import { PaymentModule } from "../payment/payment.module";
 import { OrderController } from "./order.controller";
@@ -8,7 +9,7 @@ import { OrderRepository } from "./order.repository";
 import { OrderUseCase } from "./order.use-case";
 
 @Module({
-  imports: [MenuModule, PaypayModule, PaymentModule],
+  imports: [MenuModule, PaypayModule, PaymentModule, forwardRef(() => LockerModule)],
   controllers: [OrderController],
   providers: [
     {
