23fb77cd84ab44dbd10dbd8e3c64133859b8cff2
koutyuke <gunji.kousuke08@gmail.com>
2024-01-11 11:52:18 +0900

    feat: 🎈 (api-contract) userモデルを作成

---
+++ packages/api-contracts/src/models/user.model.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+export type RoleType = "ADMIN" | "USER" | "MODERATOR";
+
+export type ProviderType = "GOOGLE" | "DISCORD";
+
+export enum RoleEnum {
+  USER = "USER",
+  ADMIN = "ADMIN",
+  MODERATOR = "MODERATOR",
+}
+
+export enum ProviderEnum {
+  GOOGLE = "GOOGLE",
+  DISCORD = "DISCORD",
+}
+
+export const roleModel = z.enum(["USER", "ADMIN", "MODERATOR"]);
+
+export const providerModel = z.enum(["GOOGLE", "DISCORD"]);
+
+export const userModel = z.object({
+  id: z.string(),
+  name: z.string(),
+  iconImage: z.string().nullable(),
+  role: roleModel,
+  provider: providerModel,
+});
