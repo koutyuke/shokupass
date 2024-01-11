2fd515e95e438373f05e8620d860196214464e0b
koutyuke <gunji.kousuke08@gmail.com>
2024-01-11 20:29:11 +0900

    feat: 🎈 (api/menu) statusの状態を変更

---
+++ apps/api/prisma/migrations/20240111112508_init/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - The values [RELEASED,UNRELEASED] on the enum `MenuStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "MenuStatus_new" AS ENUM ('PREPARATION', 'AVAILABLE', 'DELETED');
+ALTER TABLE "menus" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "menus" ALTER COLUMN "status" TYPE "MenuStatus_new" USING ("status"::text::"MenuStatus_new");
+ALTER TYPE "MenuStatus" RENAME TO "MenuStatus_old";
+ALTER TYPE "MenuStatus_new" RENAME TO "MenuStatus";
+DROP TYPE "MenuStatus_old";
+ALTER TABLE "menus" ALTER COLUMN "status" SET DEFAULT 'PREPARATION';
+COMMIT;
---
+++ supabase/migrations/20240111112720_init.sql
@@ -0,0 +1,13 @@
+alter table "public"."menus" alter column "status" drop default;
+
+alter type "public"."MenuStatus" rename to "MenuStatus__old_version_to_be_dropped";
+
+create type "public"."MenuStatus" as enum ('PREPARATION', 'AVAILABLE', 'DELETED');
+
+alter table "public"."menus" alter column status type "public"."MenuStatus" using status::text::"public"."MenuStatus";
+
+alter table "public"."menus" alter column "status" set default 'PREPARATION'::"MenuStatus";
+
+drop type "public"."MenuStatus__old_version_to_be_dropped";
+
+
