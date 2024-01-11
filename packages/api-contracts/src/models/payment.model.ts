8d9a8e48d6838b212606fb9ec125d46ebec16717
koutyuke <gunji.kousuke08@gmail.com>
2024-01-11 11:51:52 +0900

    feat: 🎈 (api-contract) paymentモデルを作成

---
+++ packages/api-contracts/src/models/payment.model.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export type PaymentStatusType = "PENDING" | "COMPLETED" | "REFUNDED" | "EXPIRED" | "FAILED";
+
+export enum PaymentStatusEnum {
+  PENDING = "PENDING",
+  COMPLETED = "COMPLETED",
+  REFUNDED = "REFUNDED",
+  EXPIRED = "EXPIRED",
+  FAILED = "FAILED",
+}
+
+export const paymentStatusModel = z.enum(["PENDING", "COMPLETED", "REFUNDED", "EXPIRED", "FAILED"]);
+
+export const paymentModel = z.object({
+  id: z.string(),
+  codeId: z.string(),
+  status: paymentStatusModel,
+  deeplink: z.string(),
+  expiresAt: z.date(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
