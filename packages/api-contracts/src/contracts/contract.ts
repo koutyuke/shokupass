f30d212d383c15bf7fa39a3eedc05833ac572d3f
koutyuke <gunji.kousuke08@gmail.com>
2024-01-17 03:03:18 +0900

    feat: 🎈 (api-contracts) 共通設定の追加

--- packages/api-contracts/src/contracts/contract.ts
+++ packages/api-contracts/src/contracts/contract.ts
@@ -1,4 +1,5 @@
 import { initContract } from "@ts-rest/core";
+import { z } from "zod";
 import { lockerRoutes } from "./locker";
 import { menuRoutes } from "./menu";
 import { orderRoutes } from "./order";
@@ -6,9 +7,16 @@ import { userRoutes } from "./user";
 
 const c = initContract();
 
-export const apiContract = c.router({
-  user: userRoutes,
-  order: orderRoutes,
-  menu: menuRoutes,
-  locker: lockerRoutes,
-});
+export const apiContract = c.router(
+  {
+    user: userRoutes,
+    order: orderRoutes,
+    menu: menuRoutes,
+    locker: lockerRoutes,
+  },
+  {
+    baseHeaders: z.object({
+      authorization: z.string(),
+    }),
+  },
+);
