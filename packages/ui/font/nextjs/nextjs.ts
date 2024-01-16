8c8fe31dfe60d4d6b9f2ea7e9a6fe39631c95d8a
koutyuke <gunji.kousuke08@gmail.com>
2024-01-17 03:08:15 +0900

    feat: ✨ (ui) nextjs用のフォント設定の作成

---
+++ packages/ui/font/nextjs/nextjs.ts
@@ -0,0 +1,10 @@
+import { M_PLUS_Rounded_1c } from "next/font/google";
+
+const mPlusRounded1c = M_PLUS_Rounded_1c({
+  weight: ["500", "700"],
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-m-plus-rounded-1c",
+});
+
+export { mPlusRounded1c };
