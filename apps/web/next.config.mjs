62c34c8c39594182c11195cbcedc99c57d0bb793
koutyuke <gunji.kousuke08@gmail.com>
2024-01-17 02:55:28 +0900

    feat: 🎈 (next.config) imageのdomainを追加

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const config = {
-  transpilePackages: ["@shokupass/ui"],
+  transpilePackages: ["@shokupass/ui", "@shokupass/api-contracts", "@mantine/core", "@mantine/hooks"],
+  images: {
+    domains: ["localhost", "127.0.0.1"],
+  }
 };
 
 export default config;
