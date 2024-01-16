8f8341a0a27fd040776d987c507770448c4a6988
koutyuke <gunji.kousuke08@gmail.com>
2024-01-17 03:56:31 +0900

    feat: ✨ (web) ts-restのfetchクライアントの作成

---
+++ apps/web/src/utils/fetch/fetchClient.ts
@@ -0,0 +1,11 @@
+import { apiContract } from "@shokupass/api-contracts";
+import { initClient } from "@ts-rest/core";
+
+const fetchClient = initClient(apiContract, {
+  baseUrl: process.env["NEXT_PUBLIC_API_BASE_URL"]!,
+  baseHeaders: {
+    "Content-Type": "application/json",
+  },
+});
+
+export { fetchClient };
