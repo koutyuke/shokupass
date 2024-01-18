2b604a74ef71cf5e322df9cf38a9e534ee0eb7b4
koutyuke <gunji.kousuke08@gmail.com>
2024-01-18 11:56:43 +0900

    chore: 🍱 (next.config) supabaseのdomainを追加

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,7 +3,14 @@ const config = {
   transpilePackages: ["@shokupass/ui", "@shokupass/api-contracts", "@mantine/core", "@mantine/hooks"],
   images: {
     domains: ["localhost", "127.0.0.1"],
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gqzkbthzeyvsmoilprak.supabase.co"
+      }
+    ]
+  },
+
 };
 
 export default config;
