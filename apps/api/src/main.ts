06068d0813f083c171c3d5343216338d28811e01
koutyuke <gunji.kousuke08@gmail.com>
2024-01-21 04:23:43 +0900

    wip: 🚧 (api) corsの設定

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from "@nestjs/core";
-import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import { AppModule } from "./app/app.module";
 import { EnvService } from "@/config/environments/env.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  const port = app.get(EnvService).Port;
-  app.enableCors({
-    origin: "*",
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
   });
+  const port = app.get(EnvService).Port;
   await app.listen(port);
 }
 bootstrap();
