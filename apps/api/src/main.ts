8114bba08368b013350b1a4bbda3e4c9009e9502
koutyuke <gunji.kousuke08@gmail.com>
2024-01-21 23:44:37 +0900

    wip: 🚧 (fastifyに変更)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import { AppModule } from "./app/app.module";
 import { EnvService } from "@/config/environments/env.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
     cors: true,
   });
   const port = app.get(EnvService).Port;
